aa8fd5930b0229b9cece5658c33f6c1155a7360e
U-AMER\coliveira6 <coliveira6@coliveira6-1.amer.globalcsc.net>
2016-10-27 13:51:56 -0200

    teste

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
+    },
+	'database': 'terraqli_bd',
+    'users_table': 'users'
+};
